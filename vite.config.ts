0fc9e933ba1ff02c66b1e83bc7aa00e11b8fb3ee
spacemanhari <hariprasath.thirupathy@gmail.com>
2026-02-20 15:26:39 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
+export default defineConfig({
+  base: '/Robert-Automobile/',
+  plugins: [react()],
+})
+
+
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
